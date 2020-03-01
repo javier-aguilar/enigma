1c0bad9f29f8ce471d3881b421d09b8d76bcb4a7
Javier Aguilar <jvr.agx@gmail.com>
2020-03-01 16:17:16 -0700

    Pass test_it_exists

---
+++ test/enigma_test.rb
@@ -0,0 +1,15 @@
+require './test/test_helper'
+require 'date'
+require './lib/enigma'
+
+class EnigmaTest < Minitest::Test
+
+  def setup
+    @enigma = Enigma.new()
+  end
+
+  def test_it_exists
+    assert_instance_of Enigma, @enigma
+  end
+
+end
