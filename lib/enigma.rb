f0462fce56ef71d5fbedf550781e3bdc4ebd9e13
Javier Aguilar <jvr.agx@gmail.com>
2020-03-01 16:18:45 -0700

    Pass test_it_can_generate_randomize_five_digits

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,2 +1,10 @@
 class Enigma
+
+  def initialize()
+  end
+
+  def randomize_five_digits
+    rand(0..99999).to_s.rjust(5, "0")
+  end
+
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -12,4 +12,11 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
+  def test_it_can_generate_randomize_five_digits
+    number = @enigma.randomize_five_digits
+
+    assert (0..99999).include?(number.to_i)
+    assert_equal 5, number.length
+  end
+
 end
