a6523f22d1addd0e43687079183e84953fcb1de6
Javier Aguilar <jvr.agx@gmail.com>
2020-03-02 10:12:40 -0700

    Remove optional parameters key and date

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -5,8 +5,8 @@ enigma = Enigma.new()
 
 source_file_path = ARGV[0]
 output_file_path = ARGV[1]
-ARGV[2].nil? ? key = enigma.randomize_five_digits : key = ARGV[2]
-ARGV[3].nil? ? date = enigma.current_date : date = ARGV[3]
+key = enigma.randomize_five_digits
+date = enigma.current_date
 
 file = File.open(source_file_path)
 text = file.read
