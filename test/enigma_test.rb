c76a69a2404c83274816efafc373934dbfd1493e
Javier Aguilar <jvr.agx@gmail.com>
2020-03-01 17:32:20 -0700

    Pass test_it_can_decrypt_with_todays_date

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -109,4 +109,14 @@ class EnigmaTest < Minitest::Test
     assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
   end
 
+  def test_it_can_decrypt_with_todays_date
+    encrypted = @enigma.encrypt("hello world", "02715")
+    expected = {
+      decryption: "hello world",
+      key: "02715",
+      date: @current_date
+    }
+    assert_equal expected, @enigma.decrypt(encrypted[:encryption], "02715")
+  end
+
 end
