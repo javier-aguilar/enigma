6f0749fd7007b96caff1b4046b038db14c3d09f8
Javier Aguilar <jvr.agx@gmail.com>
2020-03-02 10:28:53 -0700

    Check if parameters are not passed

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -2,15 +2,21 @@ require './lib/enigma'
 
 enigma = Enigma.new()
 
-source_file_path = ARGV[0]
-output_file_path = ARGV[1]
-key = ARGV[2]
-date = ARGV[3]
+def error(message)
+  puts "Please provide #{message}"
+  exit
+end
+
+ARGV[0].nil? ? (error("input source")) : source_file_path = ARGV[0]
+ARGV[1].nil? ? (error("destination source")) : output_file_path = ARGV[1]
+ARGV[2].nil? ? (error("key")) : key = ARGV[2]
+ARGV[3].nil? ? (error("date")) : date = ARGV[3]
 
 file = File.open(source_file_path)
-text = file.read
+ciphertext = file.read
 
-decryption = enigma.decrypt(text, key, date)
+decryption = enigma.decrypt(ciphertext, key, date)
 File.write(output_file_path, decryption[:decryption])
 
-puts "Created #{output_file_path} with the key #{decryption[:key]} and date #{decryption[:date]}"
+puts "Created #{output_file_path} with the key #{decryption[:key]} "\
+     "and date #{decryption[:date]}"
