1145a174956a258f5b2f30959f559ee996565cd5
Javier Aguilar <jvr.agx@gmail.com>
2020-03-02 20:55:13 -0700

    Pass test_it_can_get_expected_message_end

--- test/crackable_test.rb
+++ test/crackable_test.rb
@@ -13,4 +13,17 @@ class CrackableTest < Minitest::Test
     assert_equal expected, @enigma.last_four_characters_positions("beaweqihssi")
   end
 
+  def test_it_can_get_expected_message_end
+    expected1 = "end "
+    assert_equal expected1, @enigma.expected_message_end("beaweqihssi")
+
+    expected2 = "nd e"
+    assert_equal expected2, @enigma.expected_message_end("eaweqihssi")
+
+    expected3 = "d en"
+    assert_equal expected3, @enigma.expected_message_end("aweqihssi")
+
+    expected4 = " end"
+    assert_equal expected4, @enigma.expected_message_end("weqihssi")
+  end
 end
